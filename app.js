69abc058eba52a8b309d9b27b879645f22167fe4
Eric Lin <eric.lin88@TI03NB107>
2018-02-16 21:04:49 +0800

    initialize compete

--- app.js
+++
@@ -1,6 +0,0 @@
-new Vue({
-    el: '#app',
-    render: function (h) {
-        return h('h1', 'hello world');
-    }
-});
--- app.ts
+++
@@ -1,7 +0,0 @@
-declare var Vue: any
-new Vue({
-    el: '#app',
-    render(h) {
-        return h('h1', 'hello world')
-    }
-})
