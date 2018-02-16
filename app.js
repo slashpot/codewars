89b5d9ddd6ed00b118f8f23430414b4678f920ae
Eric Lin <eric.lin88@TI03NB107>
2018-02-16 20:26:48 +0800

    initializing

---
+++ app.js
@@ -0,0 +1,6 @@
+new Vue({
+    el: '#app',
+    render: function (h) {
+        return h('h1', 'hello world');
+    }
+});
---
+++ app.ts
@@ -0,0 +1,7 @@
+declare var Vue: any
+new Vue({
+    el: '#app',
+    render(h) {
+        return h('h1', 'hello world')
+    }
+})
